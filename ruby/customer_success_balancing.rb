4c32a7b39f2745ba77ad59c360421ba54d1d6a87
Henrique Silva <contato@henriquesilva.dev>
2021-05-27 19:05:01 -0300

    developer challenge

---
+++ ruby/customer_success_balancing.rb
@@ -0,0 +1,94 @@
+require 'minitest/autorun'
+require 'timeout'
+class CustomerSuccessBalancing
+  def initialize(customer_success, customers, customer_success_away)
+    @customer_success = customer_success
+    @customers = customers
+    @customer_success_away = customer_success_away
+  end
+
+  def execute
+    @customer_success.map do |cs| 
+      cs.merge!(count: 0)
+    end
+
+    remove_customer_success_away(@customer_success_away)
+
+    @customers.map do |c|
+      @customer_success.map do |cs|
+        if cs[:score] >= c[:score]
+            cs[:count] += 1
+        end 
+      end
+    end
+
+    if @customer_success[0][:count] == @customer_success[1][:count]
+      return 0
+    else 
+      @customer_success.sort {|a,b| a[:count] <=> b[:count]}.last[:id]
+    end
+  end
+
+  private
+
+  def remove_customer_success_away(customer_success_away)
+    customer_success_away.each do |cs_away|
+        @customer_success.delete_if { |cs| cs[:id] == cs_away}
+    end
+  end 
+end
+
+class CustomerSuccessBalancingTests < Minitest::Test
+  def test_scenario_one
+    css = [{ id: 1, score: 60 }, { id: 2, score: 20 }, { id: 3, score: 95 }, { id: 4, score: 75 }]
+    customers = [{ id: 1, score: 90 }, { id: 2, score: 20 }, { id: 3, score: 70 }, { id: 4, score: 40 }, { id: 5, score: 60 }, { id: 6, score: 10}]
+
+    balancer = CustomerSuccessBalancing.new(css, customers, [2, 4])
+    assert_equal 1, balancer.execute
+  end
+
+  def test_scenario_two
+    css = array_to_map([11, 21, 31, 3, 4, 5])
+    customers = array_to_map( [10, 10, 10, 20, 20, 30, 30, 30, 20, 60])
+    balancer = CustomerSuccessBalancing.new(css, customers, [])
+    assert_equal 0, balancer.execute
+  end
+
+  def test_scenario_three
+    customer_success = (1..999).to_a
+    customers = Array.new(10000, 998)
+
+    balancer = CustomerSuccessBalancing.new(array_to_map(customer_success), array_to_map(customers), [999])
+
+    result = Timeout.timeout(1.0) { balancer.execute }
+    assert_equal 998, result
+  end
+
+  def test_scenario_four
+    balancer = CustomerSuccessBalancing.new(array_to_map([1, 2, 3, 4, 5, 6]), array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]), [])
+    assert_equal 0, balancer.execute
+  end
+
+  def test_scenario_five
+    balancer = CustomerSuccessBalancing.new(array_to_map([100, 2, 3, 3, 4, 5]), array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]), [])
+    assert_equal 1, balancer.execute
+  end
+
+  def test_scenario_six
+    balancer = CustomerSuccessBalancing.new(array_to_map([100, 99, 88, 3, 4, 5]), array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]), [1, 3, 2])
+    assert_equal 0, balancer.execute
+  end
+
+  def test_scenario_seven
+    balancer = CustomerSuccessBalancing.new(array_to_map([100, 99, 88, 3, 4, 5]), array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]), [4, 5, 6])
+    assert_equal 3, balancer.execute
+  end
+
+  def array_to_map(arr)
+    out = []
+    arr.each_with_index { |score, index| out.push({ id: index + 1, score: score }) }
+    out
+  end
+end
+
+Minitest.run
